bc8eaa6d9960628f580c81482a22236e3dd6e2ea
Harley Jessop <harley.jessop@verizonmedia.com>
2021-02-26 16:08:03 -0700

    building out the publisher tray and footer with the new route

--- app/event/publisher/controller.js
+++ app/event/publisher/controller.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
 
 export default class PublisherController extends Controller {
+    footerCenterControls = document.getElementById('footer_center_controls');
 }
